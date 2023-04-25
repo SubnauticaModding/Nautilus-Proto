86ab4e0b3876259e6cda5000f86221a0bbd0749a
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-04-24 20:15:41 -0400

    more work on docs/frontend

--- Nautilus/Handlers/GoalHandler_Subnautica.cs
+++ Nautilus/Handlers/GoalHandler_Subnautica.cs
@@ -1,6 +1,7 @@
 using System;
 using Story;
 using UnityEngine;
+using UnityEngine.UIElements;
 using static HandReticle;
 
 namespace Nautilus.Handlers;
@@ -39,13 +40,13 @@ public static class StoryGoalHandler
 {
     /// <summary>
     /// <para>Registers a goal that is completed when an object with the given TechType is picked up, equipped, or crafted through the Mobile Vehicle Bay.</para>
-    /// <para>Important: This method can be called <b>as many times as needed</b> to add different goals to the same TechType.</para>
+    /// <para><b>Important:</b> This method can be called <b>as many times as needed</b> to add different goals to the same TechType.</para>
     /// </summary>
     /// <param name="key">The unique identifier, required for all types of StoryGoals.</param>
     /// <param name="goalType">If assigned a value other than 'Story', this will determine the automatic response to being triggered. Can add a PDA log, Radio message or Databank entry.</param>
     /// <param name="delay">StoryGoal listeners will not be notified until this many seconds after the goal is completed.</param>
     /// <param name="techType">The TechType that causes this goal to trigger, when picked up, equipped or crafted through the Mobile Vehicle Bay.</param>
-    /// <returns></returns>
+    /// <returns>The registered <see cref="ItemGoal"/>.</returns>
     public static ItemGoal RegisterItemGoal(string key, StoryGoalType goalType, float delay, TechType techType)
     {
         var goal = new ItemGoal() { key = key, goalType = goalType.GetActualValue(), delay = delay, techType = techType };
@@ -59,7 +60,7 @@ public static class StoryGoalHandler
     /// <param name="delay">StoryGoal listeners will not be notified until this many seconds after the goal is completed.</param>
     /// <param name="biomeName">The biome that must be entered to trigger this goal.</param>
     /// <param name="minStayDuration">The minimum amount of time the player must stay in the given biome.</param>
-    /// <returns></returns>
+    /// <returns>The registered <see cref="BiomeGoal"/>.</returns>
     public static BiomeGoal RegisterBiomeGoal(string key, StoryGoalType goalType, float delay, string biomeName, float minStayDuration)
     {
         var goal = new BiomeGoal() { key = key, goalType = goalType.GetActualValue(), delay = delay, biome = biomeName, minStayDuration = minStayDuration };
@@ -71,23 +72,48 @@ public static class StoryGoalHandler
     /// <param name="key">The unique identifier, required for all types of StoryGoals.</param>
     /// <param name="goalType">If assigned a value other than 'Story', this will determine the automatic response to being triggered. Can add a PDA log, Radio message or Databank entry.</param>
     /// <param name="delay">StoryGoal listeners will not be notified until this many seconds after the goal is completed.</param>
-    /// <param name="position"></param>
-    /// <param name="range"></param>
-    /// <param name="minStayDuration"></param>
-    /// <returns></returns>
+    /// <param name="position">The center of the sphere that must be occupied.</param>
+    /// <param name="range">The radius of the sphere that must be occupied.</param>
+    /// <param name="minStayDuration">The minimum amount of time the player must stay for this goal to be completed.</param>
+    /// <returns>The registered <see cref="LocationGoal"/>.</returns>
     public static LocationGoal RegisterLocationGoal(string key, StoryGoalType goalType, float delay, Vector3 position, float range, float minStayDuration)
     {
-        var goal = new LocationGoal() { key = key,}
+        var goal = new LocationGoal() { key = key, goalType = goalType.GetActualValue(), delay = delay, position = position, range = range, minStayDuration = minStayDuration };
     }
 
+    /// <summary>
+    /// Registers a goal that is completed when all required "precondition" goals have been completed
+    /// </summary>
+    /// <param name="key">The unique identifier, required for all types of StoryGoals.</param>
+    /// <param name="goalType">If assigned a value other than 'Story', this will determine the automatic response to being triggered. Can add a PDA log, Radio message or Databank entry.</param>
+    /// <param name="delay">StoryGoal listeners will not be notified until this many seconds after the goal is completed.</param>
+    /// <param name="requiredGoals">The list of all goals that must be completed before this goal is marked as complete.</param>
+    /// <returns>The registered <see cref="CompoundGoal"/>.</returns>
     public static CompoundGoal RegisterCompoundGoal(string key, StoryGoalType goalType, float delay, params string[] requiredGoals)
     {
-        
+        var goal = new CompoundGoal() { key = key, goalType = goalType.GetActualValue(), delay = delay, preconditions = requiredGoals };
     }
 
+    /// <summary>
+    /// <para>Registers a new <see cref="OnGoalUnlock"/> object for an existing goal. Handles complex actions that occur with the goal's completion.</para>
+    /// <para><b>Important:</b> Since these are stored in a dictionary, only <b>one</b> <see cref="OnGoalUnlock"/> object can be added for each specific goal key. Therefore, be careful when adding unlock data to base-game features.</para>
+    /// </summary>
+    /// <param name="goal">The goal that is associated with this action.</param>
+    /// <param name="blueprints"></param>
+    /// <param name="signals"></param>
+    /// <param name="items"></param>
+    /// <param name="achievements"></param>
+    /// <returns>The registered <see cref="OnGoalUnlock"/> object.</returns>
     public static OnGoalUnlock RegisterOnGoalUnlockData(string goal, UnlockBlueprintData[] blueprints = null, UnlockSignalData[] signals = null, UnlockItemData[] items = null, GameAchievements.Id[] achievements = null)
     {
-
+        var obj = new OnGoalUnlock()
+        {
+            goal = goal,
+            blueprints = blueprints ?? new UnlockBlueprintData[0],
+            signals = signals ?? new UnlockSignalData[0],
+            items = items ?? new UnlockItemData[0],
+            achievements = achievements ?? new GameAchievements.Id[0]
+        };
     }
 
     public static void RegisterStoryGoalCustomEvent(string goalName, Action actionOnComplete)
@@ -118,7 +144,7 @@ public enum StoryGoalType
     /// </summary>
     Encyclopedia,
     /// <summary>
-    /// Generic GoalType with no default effects. Primarily used for tracking story progress, or advanced systems such as <see cref="StoryGoalCustomEventHandler"/>, <see cref="OnGoalUnlockTracker"/> and <see cref="CompoundGoalTracker"/>.
+    /// Generic GoalType with no default effects. Primarily used for tracking story progress. Also can be used in more advanced systems such as the <see cref="StoryGoalCustomEventHandler"/>, <see cref="OnGoalUnlockTracker"/> and <see cref="CompoundGoalTracker"/>.
     /// </summary>
     Story
 }
